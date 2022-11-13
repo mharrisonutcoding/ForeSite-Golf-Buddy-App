15485eb0fa0041966f14f28493a5eadbad67eef6
darrelwu <darrelwu@utexas.edu>
2022-11-12 20:25:57 -0600

    added a tailwind config js

---
+++ config/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'green-light': '#22c55e',
+        'green-dark': '#166534',
+        'gray-dark': '#374151',
+        'gray': '#52525b',
+        'slate': '#475569',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    }
+  }
