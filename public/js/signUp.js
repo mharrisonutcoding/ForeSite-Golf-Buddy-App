520fa674ac731746ad9cd85bc518b2fe632839c5
Mike Harrison <mharrisonutcoding@gmail.com>
2022-11-14 20:00:35 -0600

    added chat handlebar file and modified logout.js file

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,25 +1,32 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ gender, userName, firstName, lastName, email, password,}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  event.preventDefault();
 
-  document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        gender,
+        userName,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
