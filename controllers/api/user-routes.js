58e98b9f82bc4be48e8b80b84533e67aad89b4d0
Mike Harrison <mharrisonutcoding@gmail.com>
2022-11-15 20:59:20 -0600

    updating routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const {User,Attribute} = require('../../models/User');
+const withAuth = require('../../utils/auth');
 
 // GET all users
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll();
+    const userData = await User.findAll({
+      attributes: { exclude: ['password']}
+    });
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
@@ -14,7 +17,7 @@ router.get('/', async (req, res) => {
 // CREATE a new user
 router.post('/', async (req, res) => {
   try {
-    const newUserData = await {User, Attribute}.create({
+    const newUserData = await User.create({
       first_name: req.body.firstname,
       last_name: req.body.username,
       email: req.body.username,
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User, Attribute} = require('../models')
+const withAuth = require("../utils/auth");
 
 router.get('/', async (req, res) => {
     try {
